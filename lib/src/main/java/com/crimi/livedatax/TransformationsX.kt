ab8f28f446d0bc9d803a10000349a416e1658e0d
Alex Crimi <>
2019-09-18 12:45:12 -0700

    Rename mapAny -> mapLatest, add mapAll

--- lib/src/main/java/com/crimi/livedatax/TransformationsX.kt
+++ lib/src/main/java/com/crimi/livedatax/TransformationsX.kt
@@ -6,12 +6,28 @@ import androidx.lifecycle.MediatorLiveData
 import androidx.lifecycle.Observer
 
 object TransformationsX {
-    fun <X, Y> mapAny(
-        vararg triggers: LiveData<X>,
+    fun <X, Y> mapLatest(
+        vararg triggers: LiveData<out X>,
         func: (X?) -> Y?
     ): LiveData<Y> {
         val result = MediatorLiveData<Y>()
         val observer = Observer<X> { x -> result.value = func.invoke(x) }
+
+        for (trigger in triggers) {
+            result.addSource(trigger, observer)
+        }
+
+        return result
+    }
+
+    fun <X, Y> mapAll(
+        vararg triggers: LiveData<out X>,
+        func: (List<X?>) -> Y?
+    ): LiveData<Y> {
+        val result = MediatorLiveData<Y>()
+        val observer = Observer<X> { _ ->
+            result.value = func.invoke(triggers.map { it.value })
+        }
 
         for (trigger in triggers) {
             result.addSource(trigger, observer)
